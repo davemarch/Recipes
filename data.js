a8b02b52321eb181c9ab0eb4219c65c53fcacdf0
davemarch <davemarchaudio@gmail.com>
2020-05-31 16:28:20 +0100

    data file update

---
+++ data.js
@@ -0,0 +1,64 @@
+
+const myArray = [
+        {
+    Picture: "https://img.hellofresh.com/f_auto,fl_lossy,q_auto,w_1200/hellofresh_s3/image/hoisin-chicken-fajitas-5674df14.jpg",
+    Food: 'Fajitas',
+    Recipe: ["Chicken", "Peppers", "Onions", "Wraps","Lettuce"],
+    Method: ["Fry"]
+    },
+        {  
+    Picture: "https://www.recipetineats.com/wp-content/uploads/2019/02/Pesto-Pasta_2-1.jpg?resize=650,910",
+    Food: "Pesto Pasta",
+    Recipe: ["Chicken", "Pesto", "Pasta", "Olive Oil"],
+    Method: ["Boil Pasta", "Fry Chicken", "Combine"]
+    }      ,
+    {
+    Picture: "https://www.lecremedelacrumb.com/wp-content/uploads/2017/09/pasta-e-fagioli-107.jpg",
+    Food: "Pasta E Fagoli",
+    Recipe: ["1 Tbs of Extra Virgin Olive Oil","2 Whole Carrots","4 Celery Stalks","1 Onion","2 cans of chopped tomatoes","2 measures veg stock", "2 bay leaves", "1 tsp dried oregano", "tsp dried basil", "1/2 tsp dried thyme", "1 can of cannellini beans", "1 can of red kidney beans", "1 cup of pasta"],
+    Method: ["1. Put all ingredients in slowcooker except beans and pasta", "2. Cook on low for 7-8 hours or high for 3-4", "3. 30 mins before serving add pasta and beans"]},
+    
+    {
+    Picture: "https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/71/527501179e9319cf888f/letterbox_Summer_20paella.jpg",
+    Food: "Paella",
+    Recipe: ["2 Garlic Cloves","Chorizo Substitute","6 x Spring Onions or 1 x Onions","200g of Paella Rice","1 x Red Pepper","100g of Frozen Peas", "Fresh Tomatoes", "1 tsp paprika", "1 tsp tumeric", "650 ml of veg stock"],
+    Method: ["Fry the garlic and chorizo", "Add onions, tumeric, rice and red pepper - For 2-3 minutes until rice is opaque", "Add the stock and simmer for 10 minutes", "Add peas and tomatoes and simmer for another 10 minutes."]
+    }
+  ,
+  {
+    Picture: "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/user-collections/my-colelction-image/2015/12/recipe-image-legacy-id--901451_9.jpg",
+    Food: "Yorkshire Puddings",
+    Recipe: ["140g - Plain Flour", "4x Eggs", "200ml - Milk", "Sunflour Oil"],
+    Method: ["Oven at 210C Fan", "", "",]
+    }
+  ,
+  {
+    Picture: "https://keviniscooking.com/wp-content/uploads/2017/10/Turkish-Spice-Blend1.jpg",
+    Food: "Montreal Steak Spice Blend",
+    Recipe: ["2 Tbsp - Sea Salt", "2 Tbps - Black Peppercorns", "1 tsp - Caraway Seeds", "1 tsp - Coriander Seeds", "1 tsp - Cumin Seeds", "1 tsp - Fennel Seeds", " 1 tsp - Celery Seeds" ],
+    Method: ["Blend together, one at a time to get consistency", "Add celery seeds at the end"]
+    },
+  
+    {
+      Picture: "https://www.simplyrecipes.com/wp-content/uploads/2014/07/hard-boiled-eggs-horiz-800-600x400.jpg",
+      Food: "Boiled Eggs",
+      Recipe: ["2 Tbsp - Sea Salt", "2 Tbps - Black Peppercorns", "1 tsp - Caraway Seeds", "1 tsp - Coriander Seeds", "1 tsp - Cumin Seeds", "1 tsp - Fennel Seeds", " 1 tsp - Celery Seeds" ],
+      Method: ["Blend together, one at a time to get consistency", "Add celery seeds at the end"]
+      },
+  
+      {
+        Picture: "https://www.simplyrecipes.com/wp-content/uploads/2014/07/grilled-corn-on-the-cob-vertical-600-600x856.jpg",
+        Food: "Corn on the Cob",
+        Recipe: ["2 Tbsp - Sea Salt", "2 Tbps - Black Peppercorns", "1 tsp - Caraway Seeds", "1 tsp - Coriander Seeds", "1 tsp - Cumin Seeds", "1 tsp - Fennel Seeds", " 1 tsp - Celery Seeds" ],
+        Method: ["Blend together, one at a time to get consistency", "Add celery seeds at the end"]
+        },
+  
+        {
+          Picture: "https://www.simplyrecipes.com/wp-content/uploads/2018/07/Guacamole-LEAD-1-600x840.jpg",
+          Food: "Guacamole",
+          Recipe: ["2 Tbsp - Sea Salt", "2 Tbps - Black Peppercorns", "1 tsp - Caraway Seeds", "1 tsp - Coriander Seeds", "1 tsp - Cumin Seeds", "1 tsp - Fennel Seeds", " 1 tsp - Celery Seeds" ],
+          Method: ["Blend together, one at a time to get consistency", "Add celery seeds at the end"]
+          },
+  
+  ]
+    ;
